8748dbddc8a5257365c99b6ae4df2d937200763b
Pieter van Ginkel <pvginkel@gmail.com>
2022-12-03 15:00:06 +0100

    Fade the led when turning the lamp on or off.

--- ESP01Lamp.ino
+++ ESP01Lamp.ino
@@ -6,10 +6,10 @@
 #include "Support.h"
 
 #define TOGGLE_DIRECTION_MS 2000
-#define TURN_OFF_MS 600
+#define DIMMING_DELAY_MS 600
 
-
-#define IS_MOSFET true
+// Invert the level the led is set to.
+#define INVERT false
 #define SCALE false
 
 #if SCALE
@@ -20,6 +20,8 @@
 #define LEVEL_MAX 255
 #define STEP_MS 15
 #endif
+
+#define FADE_STEP_MS 3
 
 #ifdef ESP8266
 #define GPIO0 0
@@ -33,15 +35,18 @@
 #define IO_LED GPIO0
 #define IO_PB RX
 
-#define INVERT (!IS_MOSFET)
 #else
 #define IO_LED 6
 #define IO_PB 4
 
-#define INVERT false
-
 typedef unsigned long time_t;
 #endif
+
+enum class Dimming {
+	None,
+	GoingUp,
+	GoingDown
+};
 
 bool isOn = false;
 bool wasOn = false;
@@ -51,10 +56,13 @@ Bounce bounce(IO_PB, 50);
 time_t lastDownChange = 0;
 time_t lastLevelChange = 0;
 bool isDown = false;
-bool goingUp = true;
-bool haveDimmed = false;
+bool isFading = false;
+time_t fadeLastMillis = 0;
+int fadeTarget = 0;
+int fadeLevel;
+auto dimming = Dimming::None;
 
-static void setLevel(int level);
+static void setLevel(int value, bool fade = false);
 
 void setup() {
 	while (!Serial);
@@ -88,6 +96,31 @@ void setup() {
 }
 
 void loop() {
+	if (isFading) {
+		auto currentMillis = millis();
+		if (currentMillis - fadeLastMillis >= FADE_STEP_MS) {
+			if (fadeTarget == fadeLevel) {
+				DEBUG(F("Ended fading"));
+				isFading = false;
+			}
+			else {
+				fadeLastMillis = currentMillis;
+
+				if (fadeTarget < fadeLevel) {
+					fadeLevel--;
+				}
+				else {
+					fadeLevel++;
+				}
+			}
+
+			setLevel(fadeLevel);
+		}
+
+		// Disable the rest of the algorithm while fading.
+		return;
+	}
+
 	bounce.update();
 
 	if (bounce.read()) {
@@ -96,17 +129,19 @@ void loop() {
 		if (!isDown) {
 			DEBUG(F("Staring down"));
 
-			if (currentMillis - lastDownChange < TOGGLE_DIRECTION_MS && haveDimmed) {
-				goingUp = !goingUp;
+			if (currentMillis - lastDownChange < TOGGLE_DIRECTION_MS) {
+				// Initializes Dimming::None to Dimming::GoingUp also.
+				dimming = dimming == Dimming::GoingUp
+					? Dimming::GoingDown
+					: Dimming::GoingUp;
 
-				DEBUG(F("Toggling going up to "), goingUp);
+				DEBUG(F("Toggling dimming direction to "), dimming == Dimming::GoingUp ? F("GoingUp") : F("GoingDown"));
 			}
 			else {
-				goingUp = true;
+				dimming = Dimming::GoingUp;
 			}
 
 			isDown = true;
-			haveDimmed = false;
 			lastDownChange = currentMillis;
 			lastLevelChange = currentMillis;
 			wasOn = isOn;
@@ -114,23 +149,21 @@ void loop() {
 			if (!isOn) {
 				DEBUG(F("Turning on"));
 
-				isOn = true;
-				goingUp = true;
-
 				if (level == 0) {
 					level = 5;
 				}
-				setLevel(level);
+				setLevel(level, true);
+
+				isOn = true;
+				dimming = Dimming::None;
 			}
 		}
 
 		if (
-			currentMillis - lastDownChange >= TURN_OFF_MS &&
+			currentMillis - lastDownChange >= DIMMING_DELAY_MS &&
 			currentMillis - lastLevelChange >= STEP_MS
 		) {
-			haveDimmed = true;
-
-			if (goingUp) {
+			if (dimming == Dimming::GoingUp) {
 				if (level < LEVEL_MAX) {
 					DEBUG(F("Stepping to "), level);
 
@@ -145,7 +178,7 @@ void loop() {
 					setLevel(level);
 				}
 			}
-			else {
+			else if (dimming == Dimming::GoingDown) {
 				if (level > 1) {
 					DEBUG(F("Stepping to "), level);
 
@@ -162,11 +195,12 @@ void loop() {
 			DEBUG(F("End down"));
 
 			auto currentMillis = millis();
-			if (currentMillis - lastDownChange < TURN_OFF_MS && wasOn) {
+			if (currentMillis - lastDownChange < DIMMING_DELAY_MS && wasOn) {
 				DEBUG(F("Turning off"));
 
+				setLevel(0, true);
+
 				isOn = false;
-				setLevel(0);
 			}
 			else {
 				DEBUG(F("Writing current level "), level);
@@ -186,24 +220,40 @@ void loop() {
 	}
 }
 
-void setLevel(int level) {
+void setLevel(int value, bool fade) {
+	if (fade) {
+		isFading = true;
+		fadeLastMillis = 0; // Trigger the algorithm on the first loop.
+		fadeTarget = value;
+		fadeLevel = isOn ? level : 0;
+#if INVERT
+		fadeLevel = 255 - fadeLevel;
+		fadeTarget = 255 - fadeTarget;
+#endif
+
+		DEBUG(F("Start fading from "), fadeLevel, F(" to "), fadeTarget);
+
+		// Let the fade algorithm manage the rest.
+		return;
+	}
+
 #if SCALE
-	float fraction = level / ((float)(LEVEL_MAX));
+	float fraction = value / ((float)(LEVEL_MAX));
 
 	// Use a parabolic function to make the level more natural.
 	fraction = fraction * fraction;
 
-	int actualLevel = (int)(fraction * 255);
-	if (actualLevel == 0 && level > 0) {
-		actualLevel = 1;
+	int actualactualValueLevel = (int)(fraction * 255);
+	if (actualValue == 0 && value > 0) {
+		actualValue = 1;
 	}
 #else
-	int actualLevel = level;
+	int actualValue = value;
 #endif
 
 #if INVERT
-	actualLevel = 255 - actualLevel;
+	actualValue = 255 - actualValue;
 #endif
 	 
-	analogWrite(IO_LED, actualLevel);
+	analogWrite(IO_LED, actualValue);
 }
